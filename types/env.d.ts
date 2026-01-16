061d803ad5082dfc84521df64bda4b24342ea949
Ian <105128049+minijae011030@users.noreply.github.com>
2026-01-16 10:14:30 +0900

    chore: env 추가

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,8 +1,14 @@
-// 예시 타입
 declare namespace NodeJS {
   interface ProcessEnv {
-    DATABASE_URL: string
-    JWT_SECRET: string
-    NEXT_PUBLIC_API_URL: string
+    NEXT_PUBLIC_API: string
+    NEXT_PUBLIC_GITHUB_REPO: string
+    NEXT_PUBLIC_BLOG_ID: string
+
+    NEXT_PUBLIC_FIREBASE_API_KEY: string
+    NEXT_PUBLIC_FIREBASE_APP_ID: string
+    NEXT_PUBLIC_AUTH_DOMAIN: string
+    NEXT_PUBLIC_PROJECT_ID: string
+    NEXT_PUBLIC_STORAGE_BUCKET: string
+    NEXT_PUBLIC_MESSAGING_SENDER_ID: string
   }
 }
