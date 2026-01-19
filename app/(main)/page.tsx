2e2019dca207b9049989bdc716080d55c8136ec2
Ian <105128049+minijae011030@users.noreply.github.com>
2026-01-19 10:48:36 +0900

    feat: 고정글 렌더링

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,7 +1,8 @@
-import { getRecentPostList } from '@/entities/post/post.api'
+import { getPinnedPostList, getRecentPostList } from '@/entities/post/post.api'
 import { MainPage } from '@/pages/main-page'
 
 export default async function Home() {
   const posts = await getRecentPostList({ page: 1, size: 5 })
-  return <MainPage posts={posts.posts} />
+  const pinnedPosts = await getPinnedPostList({ page: 1, size: 5 })
+  return <MainPage posts={posts.posts} pinnedPosts={pinnedPosts.posts} />
 }
--- entities/post/post.api.ts
+++ entities/post/post.api.ts
@@ -9,6 +9,19 @@ export async function getRecentPostList({
   size,
 }: PostListParams): Promise<PaginatedResponse<Post>> {
   const res = await API.get<ApiResponse<PaginatedResponse<Post>>>('/post', {
+    next: { revalidate: 5 * 60 },
+    params: { page, size },
+  })
+  if (res.status !== 200) throw new Error(res.message)
+  return res.data
+}
+
+export async function getPinnedPostList({
+  page,
+  size,
+}: PostListParams): Promise<PaginatedResponse<Post>> {
+  const res = await API.get<ApiResponse<PaginatedResponse<Post>>>('/post/pinned', {
+    next: { revalidate: 5 * 60 },
     params: { page, size },
   })
   if (res.status !== 200) throw new Error(res.message)
