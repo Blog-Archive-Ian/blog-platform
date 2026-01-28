cac3b1cb922c2b484376cdd93304875d072bdaa1
Ian <105128049+minijae011030@users.noreply.github.com>
2026-01-28 13:50:19 +0900

    feat(bo): 로그인 페이지 토큰 체크 로직 추가

--- apps/bo/src/routes/__root.tsx
+++ apps/bo/src/routes/__root.tsx
@@ -1,7 +1,17 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { queryClient } from '@/shared/providers/query-provider'
+import { authCheckQueryOptions } from '@/shared/query-hook/user.query'
+import { Outlet, createRootRoute, redirect } from '@tanstack/react-router'
 import * as React from 'react'
 
 export const Route = createRootRoute({
+  beforeLoad: async () => {
+    const cached = queryClient.getQueryData(authCheckQueryOptions.queryKey)
+    if (cached) return
+
+    const res = await queryClient.ensureQueryData(authCheckQueryOptions)
+
+    if (res.status === 200) throw redirect({ to: '/dashboard', replace: true })
+  },
   component: RootComponent,
 })
 
