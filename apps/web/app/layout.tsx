6e8da8b4dea61d373e60bad047ec0bd7084e87d9
Ian <105128049+minijae011030@users.noreply.github.com>
2026-02-02 19:40:52 +0900

    feat(web): 전역 metadata 추가

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -4,8 +4,25 @@ import { QueryProvider } from '../shared/providers/query-provider'
 import { ThemeProvider } from '../shared/providers/theme-provider'
 
 export const metadata: Metadata = {
-  title: 'Archive | 기술블로그',
+  title: {
+    default: 'Archive | 기술블로그',
+    template: '%s | Ian Blog',
+  },
   description: "Ian's Tech Blog",
+  metadataBase: new URL('https://blog.minjae-dev.com'),
+  openGraph: {
+    type: 'website',
+    siteName: 'Archive | Ian Blog',
+    title: 'Archive | 기술블로그',
+    images: ['/og.png'],
+    description: '프론트엔드 기술 블로그입니다. 다양한 기술 스택과 개발 경험을 공유합니다.',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Archive | Ian Blog',
+    images: ['/og.png'],
+    description: '프론트엔드 기술 블로그입니다. 다양한 기술 스택과 개발 경험을 공유합니다.',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
